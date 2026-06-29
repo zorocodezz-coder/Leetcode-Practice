e6b2dd5a37f667036c8d6a1a8ef25f2f418bf3a1
ZoroCodezz <zorocodezz@gmail.com>
2026-06-29 21:40:37 +0530

    Time: 26 ms (8.01%), Space: 51.4 MB (5.1%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
+    struct trie{
+        trie* children[26];
+        trie(){
+            for(int i=0;i<26;i++){
+                children[i]=nullptr;
+            }
+        }
+    };
+
+    void build_trie(string & s,trie* root){
+        trie* curr=root;
+
+        for(char &c:s){
+            int idx = c-'a';
+            if(!curr->children[idx]){
+                curr->children[idx]=new trie();
+            }
+            curr = curr->children[idx];
+        }
+    }
+    int fun(string s, trie* root){
+        trie* curr = root;
+        for(char c:s){
+            int idx = c-'a';
+            if(curr->children[idx]==nullptr)return 0;
+            curr=curr->children[idx];
+        }
+        return 1;
+    }
     int numOfStrings(vector<string>& patterns, string word) {
-        // generate all substring from word
-        unordered_set<string> allSubstring;
-        
-        for(int i=0;i<word.size();i++){
-            string temp="";
-            for(int j=i;j<word.size();j++){
-                temp+=word[j];
-                allSubstring.insert(temp);
-            }
+        trie* root=new trie();
+      for(int i=0;i<word.size();i++){
+        string temp="";
+        for(int j=i;j<word.size();j++){
+            temp+=word[j];
         }
-        int count=0;
-        for(string s:patterns){
-            if(allSubstring.count(s)>0){
-                count++;
-            }
-        }
-        return count;
+        build_trie(temp,root);
+      }
+      int count=0;
+      for(auto str:patterns){
+       count+= fun(str,root);
+      }
+      return count;
     }
 };
