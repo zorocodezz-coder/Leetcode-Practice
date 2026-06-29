1c44a6b5029a9c51496b4cc16b582b5c72b943aa
ZoroCodezz <zorocodezz@gmail.com>
2026-06-29 20:49:16 +0530

    Time: 45 ms (5.34%), Space: 29.8 MB (5.1%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
-    int fun(string& pat, string& word) {
-        int m = pat.size();
-        int n = word.size();
-
-        vector<int> lps(m, 0);
-        int i = 1, j = 0;
-        while (i < m) {
-            if (pat[i] == pat[j]) {
-                lps[i] = j + 1;
-                i++;
-                j++;
-            } else {
-                if (j == 0) {
-                    lps[i] = 0;
-                    i++;
-                } else {
-                    j = lps[j - 1];  
-                }
-            }
-        }
-
-        
-        int count = 0;
-        i = 0; 
-        j = 0; 
-        while (i < n) {
-            if (word[i] == pat[j]) {
-                i++;
-                j++;
-            }
-            if (j == m) {        
-                count++;
-                j = lps[j - 1];    
-            } else if (i < n && word[i] != pat[j]) {
-                if (j == 0) i++;   
-                else j = lps[j - 1];
-            }
-        }
-        return count;
-    }
-
     int numOfStrings(vector<string>& patterns, string word) {
-        int count = 0;
-        for (auto& str : patterns) {
-            if (fun(str, word) > 0) 
+        // generate all substring from word
+        unordered_set<string> allSubstring;
+        
+        for(int i=0;i<word.size();i++){
+            string temp="";
+            for(int j=i;j<word.size();j++){
+                temp+=word[j];
+                allSubstring.insert(temp);
+            }
+        }
+        int count=0;
+        for(string s:patterns){
+            if(allSubstring.count(s)>0){
                 count++;
+            }
         }
         return count;
     }
